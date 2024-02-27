27a36e5305b14fe51abe6bdc86643508d7d0fb4a
Dion Dokter <diondokter@gmail.com>
2024-02-27 13:55:59 +0100

    Remove custom alloc and tinyrlibc

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 #![no_std]
 
-use talc::{ClaimOnOom, Span, Talc, Talck};
-
 /// Configuration for the XM125
 pub mod config;
 mod detector;
@@ -14,18 +12,3 @@ pub mod radar;
 mod rss_bindings;
 /// Module to control the XM125 sensor
 pub mod sensor;
-
-static mut ARENA: [u8; 10000] = [0; 10000];
-
-#[global_allocator]
-static ALLOCATOR: Talck<spin::Mutex<()>, ClaimOnOom> = Talc::new(unsafe {
-    // if we're in a hosted environment, the Rust runtime may allocate before
-    // main() is called, so we need to initialize the arena automatically
-    ClaimOnOom::new(Span::from_const_array(core::ptr::addr_of!(ARENA)))
-})
-.lock();
-
-extern "C" {
-    #[allow(dead_code)]
-    fn snprintf(buf: *mut i8, len: u32, fmt: *const i8, ...) -> i32;
-}
