0f090c9ff47bae4391840664219a60b378df2df8
Noah <noah@superuser.sh>
2024-03-01 20:50:52 -0500

    less broken

--- rss/include/acc_alg_basic_utils.h
+++ rss/include/acc_alg_basic_utils.h
@@ -11,14 +11,13 @@
 #define ACC_ALG_SPEED_OF_LIGHT 299792458.0f
 
 #ifndef M_PI
-	#define M_PI 3.14159265358979323846
+#define M_PI 3.14159265358979323846
 #endif
 
 /*
  * The mathematical constant pi.
  */
 #define ACC_ALG_BASIC_MATH_PI M_PI
-
 
 /**
  * @brief Calculate CRC32 checksum on byte array
@@ -30,18 +29,16 @@
  */
 uint32_t acc_alg_basic_util_crc32(const uint8_t *input, size_t len);
 
-
 /**
  * @brief Calculate length of 32-bit array to contain size number of bits
  *
  * @param number_of_bits Number of bits to contain in bit array
  * @return Length of 32-bit array
  */
-static inline size_t acc_alg_basic_utils_calculate_length_of_bitarray_uint32(size_t number_of_bits)
-{
-	return (number_of_bits + (32U - 1U)) / 32U;
+static inline size_t
+acc_alg_basic_utils_calculate_length_of_bitarray_uint32(size_t number_of_bits) {
+  return (number_of_bits + (32U - 1U)) / 32U;
 }
-
 
 /**
  * @brief Set bit in bit array
@@ -49,11 +46,11 @@ static inline size_t acc_alg_basic_utils_calculate_length_of_bitarray_uint32(siz
  * @param[in, out] bitarray Array to set bit in
  * @param[in] bit_index Index of bit to set
  */
-static inline void acc_alg_basic_utils_set_bit_bitarray_uint32(uint32_t *bitarray, size_t bit_index)
-{
-	bitarray[bit_index / 32U] |= (uint32_t)1U << (bit_index & 0x1FU);
+static inline void
+acc_alg_basic_utils_set_bit_bitarray_uint32(uint32_t *bitarray,
+                                            size_t bit_index) {
+  bitarray[bit_index / 32U] |= (uint32_t)1U << (bit_index & 0x1FU);
 }
-
 
 /**
  * @brief Clear bit in bit array
@@ -61,11 +58,11 @@ static inline void acc_alg_basic_utils_set_bit_bitarray_uint32(uint32_t *bitarra
  * @param[in, out] bitarray Array to clear bit in
  * @param[in] bit_index Index of bit to clear
  */
-static inline void acc_alg_basic_utils_clear_bit_bitarray_uint32(uint32_t *bitarray, size_t bit_index)
-{
-	bitarray[bit_index / 32U] &= ~((uint32_t)1U << (bit_index & 0x1FU));
+static inline void
+acc_alg_basic_utils_clear_bit_bitarray_uint32(uint32_t *bitarray,
+                                              size_t bit_index) {
+  bitarray[bit_index / 32U] &= ~((uint32_t)1U << (bit_index & 0x1FU));
 }
-
 
 /**
  * @brief Check if bit is set in bit array
@@ -74,10 +71,11 @@ static inline void acc_alg_basic_utils_clear_bit_bitarray_uint32(uint32_t *bitar
  * @param[in] bit_index Index of bit to check
  * @return True if bit is set
  */
-static inline bool acc_alg_basic_utils_is_bit_set_bitarray_uint32(const uint32_t *bitarray, size_t bit_index)
-{
-	return (bitarray[bit_index / 32U] & ((uint32_t)1U << (bit_index & 0x1FU))) != 0U;
+static inline bool
+acc_alg_basic_utils_is_bit_set_bitarray_uint32(const uint32_t *bitarray,
+                                               size_t bit_index) {
+  return (bitarray[bit_index / 32U] & ((uint32_t)1U << (bit_index & 0x1FU))) !=
+         0U;
 }
-
 
 #endif
