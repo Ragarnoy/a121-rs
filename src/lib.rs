e946f8150d4d2167ffb0f5db5b3d28c8ee498892
ragarnoy <tiagolernould@gmail.com>
2024-03-03 19:33:19 +0100

    Add optional libm dependency and remove unused snprintf

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@ pub mod config;
 pub mod detector;
 /// Hardware Abstraction Layer for the XM125
 pub mod hal;
+#[cfg(feature = "libm")]
+pub mod libm;
 pub mod num;
 pub mod processing;
 pub mod radar;
@@ -15,8 +17,3 @@ pub mod radar;
 mod rss_bindings;
 /// Module to control the XM125 sensor
 pub mod sensor;
-
-extern "C" {
-    #[allow(dead_code)]
-    fn snprintf(buf: *mut i8, len: u32, fmt: *const i8, ...) -> i32;
-}
