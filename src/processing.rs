8c3512dd2abc9cedce0ed101a5a0559e54a33e15
ragarnoy <tiagolernould@gmail.com>
2024-04-16 21:04:52 +0200

    Implement conversion traits for ProcessingResult and AccComplex

--- src/processing.rs
+++ src/processing.rs
@@ -83,3 +83,19 @@ impl Drop for Processing {
         }
     }
 }
+
+impl From<acc_processing_result_t> for ProcessingResult {
+    fn from(result: acc_processing_result_t) -> Self {
+        let frame = unsafe { AccComplex::from_ptr(result.frame) };
+        Self {
+            inner: result,
+            frame,
+        }
+    }
+}
+
+impl From<ProcessingResult> for acc_processing_result_t {
+    fn from(result: ProcessingResult) -> Self {
+        result.inner
+    }
+}
